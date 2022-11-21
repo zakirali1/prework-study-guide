9cf293bf351dedb7c764aa088ebffd6ab685fb4f
Zak <zak@localhost.localdomain>
2022-11-21 09:38:12 +0000

    added JS

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+const topic = ["HTML","CSS", "Git", "Javascript"];
+
+
+for (let i = 0 ; i < topic.length; i++) {
+    console.log(topic[i]);
+};
+
+/*
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+  } else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+  } else if (topic === 'Git') {
+    console.log("Let's study Git!");
+  } else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log('Please try again!');
+  }
+
+  const shapes = ["triangle","square", "pentagon", "circle"];
+  console.log(shapes)
+
+  for (let i = 0; i < shapes.length; i++) {
+    console.log(i)
+  };
+
+
+console.log(shapes[0])
+
+*/
